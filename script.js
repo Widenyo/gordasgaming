1ccc4e8939b521ffab7a171d4670da436ea6fbad
Emiliar2002 <earancio616@gmail.com>
2022-03-15 05:39:20 -0300

    rockeando

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const blue = document.getElementById("blue")
+const fotos = document.getElementById("vanillaFotos")
 
 
 fetch("https://criptoya.com/api/dolar")
@@ -7,3 +8,17 @@ fetch("https://criptoya.com/api/dolar")
     let dolarBlue = data.blue
     blue.innerHTML += dolarBlue
 })
+
+for(i = 0; i < 6; i++){
+    fetch("https://danbooru.donmai.us/posts/random.json?tags=rating:safe+vanilla_h+solo")
+    .then(response => response.json())
+    .then(data =>{
+        if(data.file_url === undefined){
+            i--
+        }
+        else{
+            let foto = data.file_url
+            fotos.innerHTML += `<a href="${foto}" target="_blank"><img id="foto" src="${foto}"></a>`
+        }
+    })
+}
