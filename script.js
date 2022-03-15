a0f7d1192f9d3058b0849c2bde07f108ce766991
Emiliar2002 <earancio616@gmail.com>
2022-03-15 16:47:34 -0300

    mi nina bonita

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const blue = document.getElementById("blue")
 const fotos = document.getElementById("vanillaFotos")
+const miNinaBonita = ["MI NIÑA BONITA MI DULCE PRINCESA", "ME SIENTO EN LAS NUBES CUANDO TU ME BESAS", "Y SIENTO QUE VUELO MAS ALTO QUE EL CIELO", "SI TENGO DE CERCA EL OLOR DE TU PELO", "MI NIÑA BONITA BRILLANTE LUCERO", 'TE QUEDA PEQUEÑA LA FRASE TE QUIERO']
 
 
 fetch("https://criptoya.com/api/dolar")
@@ -9,6 +10,8 @@ fetch("https://criptoya.com/api/dolar")
     blue.innerHTML += dolarBlue
 })
 
+
+let j = 0
 for(i = 0; i < 6; i++){
     fetch("https://danbooru.donmai.us/posts/random.json?tags=rating:safe+vanilla_h+solo")
     .then(response => response.json())
@@ -18,7 +21,8 @@ for(i = 0; i < 6; i++){
         }
         else{
             let foto = data.file_url
-            fotos.innerHTML += `<a href="${foto}" target="_blank"><img id="foto" src="${foto}"></a>`
+            fotos.innerHTML += `<a href="${foto}" title="${miNinaBonita[j]}" target="_blank"><img id="foto" src="${foto}"></a>`
+            j++
         }
     })
 }
